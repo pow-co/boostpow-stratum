5f190a42f574fe3c1347506147694188dfc4d486
Steven Zeiler <me@stevezeiler.com>
2022-04-14 22:20:53 +0200

    fix(powco-api): specify interface for pow.co/api/v1/jobs results

--- test/powco_test.ts
+++ test/powco_test.ts
@@ -3,13 +3,13 @@ import { expect } from './utils'
 
 import { BoostPowJob } from 'boostpow'
 
-import { listJobs } from '../src/powco'
+import { listJobs, PowcoJob } from '../src/powco'
 
 describe("Calling PowCo Platform APIs", () => {
 
   it("should get a list of recent jobs", async () => {
 
-    let jobs: typeof BoostPowJob[] = await listJobs()
+    let jobs: PowcoJob[] = await listJobs()
 
     expect(jobs.length).to.be.greaterThan(0)
 
