dcc1f432b2469cd116d93601b375d1ae471d8381
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2022-04-18 12:28:48 -0500

    feat(server_session): handle configure.
    The configure message has to do with stratum extensions, the most
    important of which is version-rolling, which has to do with
    ASICBoost. We handle four types of extensions.
    
    version-rolling
    minimum-difficulty
    subscribe-extranonce
    info

--- src/server_session.ts
+++ src/server_session.ts
@@ -36,12 +36,165 @@ type ExtensionParameters = {[key: string]: JSONValue;}
 type HandleExtension = (requested: ExtensionParameters) =>
   {'reply': extension_result, 'keep'?: ExtensionParameters}
 
+type ExtensionHandlers = {[key: string]: HandleExtension}
+
+function extend(handlers?: ExtensionHandlers) {
+
+  // undefined indicates that this session does not support Stratum
+  // extensions because the configure message was never sent. Otherwise,
+  // there is a list of supported extensions.
+  let extensions: undefined | {[key: string]: ExtensionParameters}
+
+  // before the first message is sent, we don't know if we are using the
+  // extended version of the protocol.
+  let extended_protocol: undefined | boolean
+
+  // if no handlers are provided, then we are not using the extended protocol
+  if (handlers === undefined) extended_protocol = false
+
+  function parameters(name: string): ExtensionParameters {
+    if (!extended_protocol) return {}
+
+    let p = extensions[name]
+    if (!p) return {}
+
+    return p
+  }
+
+  return {
+    configured: (): boolean => {
+      return extended_protocol !== true
+    },
+    parameters: parameters,
+    // is a given extension supported?
+    supported: (name?: string): boolean => {
+      if (!extended_protocol) return false
+      if (!name) return true
+
+      let p = extensions[name]
+      if (!p) return false
+
+      return true
+    },
+    // minimum difficulty is an extension that let us remember a minimum
+    // difficulty to send to the client. If the extension is not supported,
+    // this function returns zero.
+    minimum_difficulty: (): number => {
+      let p = parameters('minimum_difficulty')['value']
+      if (p === undefined) return 0
+      return <number>p
+    },
+    handle: (requests?: extension_requests): extension_results => {
+      if (!handlers) return {}
+
+      // if we handle extensions with an undefined request, that
+      // means that extensions are not suppored for this session.
+      if (!requests) {
+        extended_protocol = false
+        return {}
+      }
+
+      extended_protocol = true
+
+      if (!extensions) extensions = {}
+      let reply: extension_results = {}
+
+      for (let key of Object.keys(requests)) {
+        let h = handlers[key]
+
+        // if we do not have a handler for a given extension,
+        // then it is not supported.
+        if (!h) {
+          reply[key] = [false, {}]
+          continue
+        }
+
+        let it = h(requests[key])
+        reply[key] = it['reply']
+
+        let keep = it['keep']
+        if (keep) extensions[key] = keep
+      }
+
+      return reply
+    }
+  }
+}
+
+// version rolling is the most important extension because it's needed to
+// support ASICBoost.
+function versionRollingHandler(mask: number): HandleExtension {
+  return (requested: ExtensionParameters) => {
+    if (Object.keys(requested).length != 2) return {reply: ['invalid parameters', {}]}
+
+    let requested_mask = requested['mask']
+    let minBitCount = requested['min-bit-count']
+    if (!requested_mask || !minBitCount || (typeof requested_mask !== 'string') ||
+      !SessionID.valid(requested_mask) || typeof minBitCount !== 'number')
+      return {reply: ['invalid parameters', {}]}
+
+    let new_mask = mask & boostpow.Int32Little.fromHex(requested_mask).number
+    let counter = new_mask
+    let bit_count = 0
+
+    for (let i = 0; i < 32; i++) {
+      if ((counter & 1) == 1) bit_count++
+      counter = counter >> 1
+    }
+
+    if (bit_count < minBitCount) return {reply: ['could not satisfy min-bit-count', {}]}
+
+    return {
+      reply: [true, {mask: new_mask}],
+      keep: {mask: new_mask}
+    }
+  }
+}
+
+// subscribe extranonce is also important because it lets us
+// change extraNonce2_size
+function subscribeExtranonceHandler(): HandleExtension {
+  return (requested: ExtensionParameters) => {
+    if (Object.keys(requested).length != 0)
+      return {reply: ['invalid parameters', {}]}
+    return {reply: [true, {}], keep: {}}
+  }
+}
+
+// minimum difficulty lets the client set a minimum difficulty with the server.
+function minimumDifficultyHandler(): HandleExtension {
+  return (requested: ExtensionParameters) => {
+    if (Object.keys(requested).length != 1) return {reply: ['invalid parameters', {}]}
+    let value = requested['value']
+    if (!value || typeof value !== 'number') return {reply: ['invalid parameters', {}]}
+    return {reply: [true, {}], keep: requested}
+  }
+}
+
+// info is an extension where the client tells the server some information
+// about itself that we don't use.
+function infoHandler(): HandleExtension {
+  return (requested: ExtensionParameters) => {
+    if (Object.keys(requested).length != 4 || !requested['connection-url'] ||
+      !requested['hw-version'] || !requested['sw-version'] || !requested['hw-id'])
+      return {reply: ['invalid parameters', {}]}
+    return {reply: [true, {}], keep: requested}
+  }
+}
+
+let extensionHandlers: ExtensionHandlers = {
+  'version_rolling': versionRollingHandler(boostpow.Utils.generalPurposeBitsMask()),
+  'minimum_difficulty': minimumDifficultyHandler(),
+  'subscribe_extranonce': subscribeExtranonceHandler(),
+  'info': infoHandler()
+}
+
 export function server_session(
   select: Subscribe,
   can_submit_without_authorization: boolean,
   // undefined indicates that Stratum extensions are not supported.
   // Otherwise there is a list of supported extensions.
-  extension_handlers?: {[key: string]: HandleExtension}
+  extension_handlers?: ExtensionHandlers
 ): Local {
   return (remote: Remote, disconnect: () => void) => {
     let running: boolean = true
@@ -51,43 +204,7 @@ export function server_session(
       disconnect()
     }
 
-    // undefined indicates that this session does not support Stratum
-    // extensions because the configure message was never sent. Otherwise,
-    // there is a list of supported extensions.
-    let extensions: undefined | {[key: string]: ExtensionParameters}
-
-    // before the first message is sent, we don't know if we are using the
-    // extended version of the protocol.
-    let extended_protocol: undefined | boolean
-
-    // which parameters do we store for a given extension?
-    function extension_parameters(name: string): ExtensionParameters {
-      if (!extended_protocol) return {}
-
-      let p = extensions[name]
-      if (!p) return {}
-
-      return p
-    }
-
-    // is a given extension supported?
-    function extension_supported(name: string): boolean {
-      if (!extended_protocol) return false
-
-      let p = extensions[name]
-      if (!p) return false
-
-      return true
-    }
-
-    // minimum difficulty is an extension that let us remember a minimum
-    // difficulty to send to the client. If the extension is not supported,
-    // this function returns zero.
-    function minimum_difficulty(): number {
-      let p = extension_parameters('minimum_difficulty')['value']
-      if (p === undefined) return 0
-      return <number>p
-    }
+    let extensions = extend(extension_handlers)
 
     // list of recent jobs.
     let jobs: StratumJob[] = []
@@ -127,7 +244,7 @@ export function server_session(
 
     // when we know of a new job, we have to send a notify message.
     function notify_new_job(j: StratumJob) {
-      if (extension_supported("subscribe_extranonce")) send_set_extranonce([id, j.extranonce2Size])
+      if (extensions.supported("subscribe_extranonce")) send_set_extranonce([id, j.extranonce2Size])
 
       // we always use the same difficulty as the job for now.
       send_set_difficulty(NotifyParams.nbits(j.notify))
@@ -154,77 +271,49 @@ export function server_session(
 
     // configure is an optional first message that determines wheher
     // extensions and supported and which ones.
+    function configure(request: parameters): StratumResponse {
+      // If extensions are not supported, then we are not using the extended
+      // protocol, we don't know about this message, and it is an error.
+      if (!extension_handlers) return {result: null, err: Error.make(Error.ILLEGAL_METHOD)}
+
+      let requested = Extensions.extension_requests(request.params)
+      if (!requested) return {result: null, err: Error.make(Error.ILLEGAL_PARARMS)}
+
+      // If we have already received the configure method, then only minimum_difficulty is allowed.
+      if (extensions.configured() && !(extensions.supported('minimum_difficulty') &&
+        requested['minimum_difficulty'] && Object.keys(requested).length === 1))
+          return {result: null, err: Error.make(Error.ILLEGAL_PARARMS)}
+
+      return {result: Extensions.configure_response_result(extensions.handle(requested)), err: null}
+    }
+
     function handleConfigure(request: request): void {
-      remote.respond(((requested: extension_requests, result, error) => {
-        // If extensions are not supported, then we don't know about this message.
-        if (!extension_handlers || extended_protocol === false)
-          return error(Error.ILLEGAL_METHOD)
-
-        if (!requested) return error(Error.ILLEGAL_PARARMS)
-
-        // If we have already received the configure method, then only minimum_difficulty is allowed.
-        if (extended_protocol === true && extension_supported('minimum_difficulty')) {
-          let min_diff = requested['minimum_difficulty']
-          if (!min_diff || Object.keys(requested).length != 1)
-            return error(Error.ILLEGAL_PARARMS)
-
-          let it = extension_handlers['minimum_difficulty'](requested['minimum_difficulty'])
-
-          let keep = it['keep']
-          if (keep) extensions['minimum_difficulty'] = keep
-
-          return result({'minimum_difficulty': it['reply']})
-        } return error(Error.ILLEGAL_METHOD)
-
-        extended_protocol = true
-        extensions = {}
-        let reply: extension_results = {}
-
-        for (let key of Object.keys(requested)) {
-          let h = extension_handlers[key]
-
-          if (!h) {
-            reply[key] = [false, {}]
-            continue
-          }
-
-          let it = h(requested[key])
-          reply[key] = it['reply']
-
-          let keep = it['keep']
-          if (keep) extensions[key] = keep
-        }
-
-        return result(reply)
-      })(Extensions.extension_requests(request.params),
-        (r: extension_results) => {
-          return {id: request.id, result: Extensions.configure_response_result(r), err: null}
-        },
-        (err: number) => {
-          return {id: request.id, result: null, err: Error.make(err)}
-        }
-      ))
+      let response: StratumResponse = configure(request.params)
+      response.id = request.id
+      remote.respond(<response>response)
+      if (response.err != null) close()
     }
 
     function handleSubscribe(request: request): void {
+
+      if (!extensions.configured()) extensions.handle();
+
       ((sub, error) => {
-        if (!sub) return error(Error.ILLEGAL_PARARMS)
-
-        if (extended_protocol === undefined) extended_protocol = false
-
         // subscribe can only be called once. -- or can it?
         if (subscribed()) return error(Error.ILLEGAL_METHOD)
+
+        if (!sub) return error(Error.ILLEGAL_PARARMS)
 
         user_agent = SubscribeRequest.userAgent(sub)
 
         // if we use subscribe_extranonce, then the result is different.
-        let subscribe_extranonce: boolean = extension_supported("subscribe_extranonce")
+        let subscribe_extranonce: boolean = extensions.supported("subscribe_extranonce")
 
         let job = select({
           'subscribe_extranonce': subscribe_extranonce,
           'new_job': notify_new_job,
           'hashpower':() => { return {'hashpower': 0, 'certainty': 0} },
-          'minimum_difficulty': minimum_difficulty,
+          'minimum_difficulty': extensions.minimum_difficulty,
           'cancel': close
         })
         if (!job) return error(Error.INTERNAL_ERROR)
@@ -255,11 +344,10 @@ export function server_session(
     }
 
     function authorize(params: parameters): StratumResponse {
+
       let auth = AuthorizeRequest.read_params(params)
 
       if (!auth) return {result: null, err: Error.make(Error.ILLEGAL_PARARMS)}
-
-      if (extended_protocol === undefined) extended_protocol = false
 
       // can only auhorize once.
       if (authorized()) return {result: null, err: Error.make(Error.ILLEGAL_METHOD)}
@@ -270,6 +358,7 @@ export function server_session(
     }
 
     function handleAuthorize(request: request): void {
+      if (!extensions.configured()) extensions.handle();
       let response: StratumResponse = authorize(request.params)
       response.id = request.id
       remote.respond(<response>response)
@@ -277,6 +366,7 @@ export function server_session(
     }
 
     function submit(request: parameters): StratumResponse {
+      if (!subscribed()) return {result: null, err: Error.make(Error.ILLEGAL_METHOD)}
       let x = Share.read(request)
       if (!x) return {result: null, err: Error.make(Error.ILLEGAL_PARARMS)}
       // TODO
